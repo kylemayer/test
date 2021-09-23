5f1af8ddef438536cd34aab3a86c96a5a21f448b
Kyle Mayer <x@kylemayer.dev>
2021-09-23 16:35:29 -0700

    date functions package

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,7 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Test
         </h1>
 
         <p className="mt-3 text-2xl">
